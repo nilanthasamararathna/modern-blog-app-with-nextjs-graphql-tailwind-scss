b4da867656fd3309c8027ef3d5bada3d5c84ed2d
E.Nilantha Samararathna <45939807+nilanthasamararathna@users.noreply.github.com>
2025-05-10 11:38:42 +0530

    Create next app, Navbar, Categories, Posts and Create GraphQL query to fetch data

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      spacing: {
+        '2/3': '66.666667%',
+      },
+    },
+  },
+  plugins: [],
+}
+
